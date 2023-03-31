b215a301e69dda90e725f9a14331c75051d8c2e7
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2023-03-30 23:26:44 -0300

    Update testeproj.c

--- testeproj.c
+++ testeproj.c
@@ -5,231 +5,151 @@
 #include <signal.h>
 #include <stdarg.h>
 #include <stdio.h>
-#include <math.h>
 
-void imprima(char *string, ...)
-{
-    // tipo de objeto completo adequado para armazenar as informações necessárias para as macros
-    va_list argumentoRecebido;
 
-    // permite o acesso a argumentos de funções variáveis
-    va_start(argumentoRecebido, string);
-
-    int size;
-    int i = 0;
-
-    while (string[i] != '\0')
-    {
-
-        if (string[i] == '%')
-        {
-
-            i++;
-            switch (string[i])
-            {
-            case 's':
-                char *palavra = va_arg(argumentoRecebido, char *);
-                size = strlen(palavra);
-                write(1, palavra, size);
-                break;
-
-            case 'c':
-                char *caracter = va_arg(argumentoRecebido, char *);
-                write(1, &caracter, 1);
-                break;
-
-            case 'd':
-            case 'i':
-                long inteiro = va_arg(argumentoRecebido, int);
-                int tamanho = snprintf(NULL, 0, "%ld", inteiro);
-                char *buffer = malloc(tamanho + 1);
-                snprintf(buffer, tamanho + 1, "%ld", inteiro);
-                write(1, buffer, sizeof(inteiro));
-                break;
-
-            case 'f':
-                double flutuante = va_arg(argumentoRecebido, double);
-                char buffer[100];
-                gcvt(flutuante, 10, buffer);
-                write(1, buffer, sizeof(flutuante));
-                break;
-
-            case '.':
-                if (string[i+2] == 0)
-                {
-                    /* code */
-                }
-                else
-                {
-                    double flutuante = va_arg(argumentoRecebido, double);
-                    int numero = (int)(string[i + 1] - '0');
-                    char buffer[100];
-                    gcvt(flutuante, contar_digitos((int)flutuante) + numero, buffer);
-                    write(1, buffer, sizeof((int)flutuante) + numero);
-                    i += 2;
-                }
-                break;
-
-            default:
-                write(1, "ERRO NO PRINTF\n", 16);
-                break;
-            }
-            if (string[i] == 's')
-            {
-
-                // char *palavra = va_arg(argumentoRecebido, char *);
-                // size = strlen(palavra);
-                // write(1, palavra, size);
-            }
-            else if (string[i] == 'd' || string[i] == 'i')
-            {
-
-                // long inteiro = va_arg(argumentoRecebido, int);
-                // int tamanho = snprintf(NULL, 0, "%ld", inteiro);
-                // char *buffer = malloc(tamanho + 1);
-                // snprintf(buffer, tamanho + 1, "%ld", inteiro);
-                // write(1, buffer, sizeof(inteiro));
-            }
-            else if (string[i] == 'c')
-            {
-                // char *caracter = va_arg(argumentoRecebido, char *);
-                // write(1, &caracter, 1);
-            }
-            else if (string[i] == 'f')
-            {
-
-                // double flutuante = va_arg(argumentoRecebido, double);
-                // char buffer[100];
-                // gcvt(flutuante, 10, buffer);
-                // write(1, buffer, sizeof(flutuante));
-            }
-            else if (string[i] == '0' && string[i + 1] == '.' && (string[i + 2] == '1' || string[i + 2] == '2' || string[i + 2] == '3') && string[i + 3] == 'f')
-            {
-
-                double flutuante = va_arg(argumentoRecebido, double);
-                int numero = (int)(string[i + 2] - '0');
-                char buffer[100];
-                gcvt(flutuante, contar_digitos((int)flutuante) + numero, buffer);
-                write(1, buffer, sizeof((int)flutuante) + numero);
-                i += 3;
-            }
-            else if (string[i] == '.' && (string[i + 1] == '1' || string[i + 1] == '2' || string[i + 1] == '3') && string[i + 2] == 'f')
-            {
-
-                // double flutuante = va_arg(argumentoRecebido, double);
-                // int numero = (int)(string[i + 1] - '0');
-                // char buffer[100];
-                // gcvt(flutuante, contar_digitos((int)flutuante) + numero, buffer);
-                // write(1, buffer, sizeof((int)flutuante) + numero);
-                // i += 2;
-            }
-        }
-        else
-        {
-
-            write(1, &string[i], 1);
-        }
-        i++;
-    }
-
-    va_end(argumentoRecebido);
+int contar_digitos(int n){
+	int count = 0;
+	while (n != 0) {
+		n = n/10;
+		++count;
+	} 
+	return count;
 }
 
-int contar_digitos(int n)
-{
-    int count = 0;
-    do
-    {
-        n /= 10;
-        ++count;
-    } while (n != 0);
-    return count;
+
+
+
+
+
+void imprima(char* frasePassada, ...){
+	// um tipo de objeto completo adequado para armazenar as informações necessárias para as macros
+	va_list args;
+
+	//Permite o acesso a argumentos de funções variáveis
+  	va_start(args, frasePassada);
+	
+	char buffer[100];
+  	int size;
+  	int i=0;
+  	while (frasePassada[i] != '\0'){
+
+		if (frasePassada[i] == '%'){
+
+	    		i++;
+				switch (frasePassada[i])
+				{
+					case 's':
+						//va_arg acessa o próximo argumento de função variádica
+						char *palavra = va_arg(args, char*);
+		      	    	size = strlen(palavra);
+		      			write(1, palavra, size);
+					break;
+
+					case 'c':
+						//va_arg acessa o próximo argumento de função variádica
+						char *caracter = va_arg(args, char*);
+						write(1, &caracter, 1);
+					break;
+
+					case 'd':case 'i':
+						//va_arg acessa o próximo argumento de função variádica
+						int inteiro = va_arg(args, int);
+						buffer[0]='\0';
+						sprintf(buffer,"%d",inteiro);
+						write(1, buffer, sizeof(inteiro));
+					break;
+
+					case 'f':
+						double flutuante = va_arg(args, double);
+						buffer[0] = '\0';
+						sprintf(buffer,"%f",flutuante);
+						write(1, buffer, sizeof(flutuante));
+					break;
+
+					case '.'://1 2 3
+					if ((frasePassada[i+1]<=51 && frasePassada[i+1]>=49) && frasePassada[i+2] == 'f')
+					{
+						double flutuante = va_arg(args, double);
+						buffer[0] = '\0';
+						char tipofloat[] = {'%','.',(char)frasePassada[i+1],'f'};
+						//write(1,tipofloat,4);
+						sprintf(buffer,tipofloat,flutuante);
+						write(1, buffer, sizeof(flutuante));
+						i= i + 2;
+					}else{
+						write(1, "Erro na sintaxe do printf", 25);
+					}
+					break;
+
+					case '0'://.1 .2 .3 todo 4 5
+					if (frasePassada[i+1] == '.' && (frasePassada[i+2]<=51 && frasePassada[i+2]>=49))
+					{
+						double flutuante = va_arg(args, double);
+						buffer[0] = '\0';
+						char tipofloat[] = {'%','0','.',(char)frasePassada[i+2],'f'};
+						//write(1,tipofloat,5);
+						sprintf(buffer,tipofloat,flutuante);
+						write(1, buffer, sizeof(flutuante));
+						i = i + 3;
+						
+					}else{
+						write(1, "Erro na sintaxe do printf", 25);
+					}
+					break;
+					
+				default:
+					write(1, "Erro na sintaxe do printf", 25);
+					break;
+				}
+
+		}else{
+
+		write(1, &frasePassada[i], 1);
+		}
+		i++;
+  	}
+
+	//termina a travessia dos argumentos da função variádica
+  	va_end(args);
 }
 
-void main()
-{
 
-    char *str = "Alo Alo";
 
-    int inteiro1 = 34657;
-    int inteiro2 = 31225;
-    int inteiro3 = 3222;
-    char caracter = 'e';
-    double flutuante = 323.2333;
-    float flutuante1 = 12.453;
-    float flutuante2 = 12.332;
 
-    imprima(" Olá mundo!!!!!\n");
 
-    imprima(" %s\n", str);
 
-    imprima(" Numeros sortidos %i \n", inteiro1);
 
-    imprima(" Numeros sortidos %c \n", caracter);
+int main(){
 
-    imprima(" Numeros sortidos %f \n", flutuante);
+	char *str = "Isso é uma string";
+	char caracter = 'M';
 
-    imprima(" Numeros sortidos %d %f %d %f %d e um caracter %c\n", inteiro1, flutuante1, inteiro2, flutuante2, inteiro3, caracter);
+	int inteiro1 = 34657;
+	int inteiro2 = 31225;
+	int inteiro3 = 3222;
 
-    imprima(" Numeros sortidos %0.3f\n", flutuante1);
+	float flutuante1 = 12.453976;
+	float flutuante2 = 12.332;
+	float flutuante3 = 323.23001;
 
-    imprima(" Numeros sortidos %.2f\n", flutuante1);
+	//EXERCICIO 1
+	imprima("TRABALHO DE SO\n");
+	imprima("%s\n\n",str);
+
+	//EXERCICIO 2
+	imprima("Inteiro %i \n",  inteiro1);
+	imprima("Inteiro %d \n",  inteiro2);
+	imprima("Caractere %c \n",  caracter);
+	imprima("Float %f \n\n",  flutuante1);
+
+	//EXERCICIO3
+	imprima("Numeros %d %f %d %f %d e caracter %c\n\n",  inteiro1, flutuante1, inteiro2, flutuante2, inteiro3, caracter);
+	
+	//EXERCICIO 4
+	imprima("Flutuante com 3 pontos %0.3f\n", flutuante1);
+	imprima("Flutuante com 2 pontos %0.2f\n",  flutuante1);
+	imprima("Flutuante com 3 pontos %0.3f\n", flutuante1);
+
+
+	return 0;
 }
-
-// #define _XOPEN_SOURCE_EXTENDED 1
-// #include <string.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <signal.h>
-// #include <stdarg.h>
-// #include <stdio.h>
-// #include <math.h>
-// int tamanhoString(char *str, ...)
-// {
-//     int i = 0;
-
-//     while (*str != '\0')
-//     {
-//         i++;
-//         str++;
-//     }
-
-//     return i;
-// }
-
-// void meuPrint(char *str)
-// {
-//     int tamanho = tamanhoString(str);
-//     write(1, str, tamanho);
-// }
-
-// void imprimir(char *string, ...)
-// {
-//     char str[100];
-//     va_list argumentos;
-//   	va_start(argumentos, string);
-
-//     // while (string != '\0')
-//     // {
-//     //     if (string == '%')
-//     //     {
-//     //         sprintf(str, string, format);
-//     //         format++;r
-//     //     }
-//     //     string++;
-//     // }
-
-//     // A função sprintf serve para formatar uma string e guardar o resultado em um array (na realidade, ela aceita um ponteiro).
-//     sprintf(str, string, format);
-//     meuPrint(str);
-
-//     return (0);
-// }
-
-// int main()
-// {
-//     float sla = 1.2345;
-//     imprimir("oi %.2f tudo bem", sla);
-
-//     return 0;
-// }
