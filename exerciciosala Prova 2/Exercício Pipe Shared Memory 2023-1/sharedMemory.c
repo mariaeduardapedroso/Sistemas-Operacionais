c5901fca91c5b32152f25d49f2e8529822d66fa7
Maria Eduarda Pedroso <mpedroso@alunos.utfpr.edu.br>
2023-05-04 08:50:43 -0300

    Update sharedMemory.c

--- exerciciosala Prova 2/Exercício Pipe Shared Memory 2023-1/sharedMemory.c
+++ exerciciosala Prova 2/Exercício Pipe Shared Memory 2023-1/sharedMemory.c
@@ -50,7 +50,6 @@ int main(int argc, char *argv[])
         strncpy(segmento, "olA", SHM_SIZE);
 
         /* Desvincular do segmento */
-        //shmdt(segmento);
         if (shmdt(segmento) == -1)
         {
             perror("shmdt");
@@ -71,7 +70,6 @@ int main(int argc, char *argv[])
         printf("\nCONTEUDO DO SEGUIMENTO %s\n", segmento);
 
         /* Desvincular do segmento */
-        //shmdt(segmento);
         if (shmdt(segmento) == -1)
         {
             perror("shmdt");
@@ -85,66 +83,3 @@ int main(int argc, char *argv[])
 
     return 0;
 }
-
-// int main(int argc, char const *argv[])
-// {
-//     key_t key;
-//     int id;
-//     char *Data;
-
-//     // APENAS CRIAR UMA CHAVE A PARTIR DESSE PARAMETRO PEGA STRING E CARACTERE EMBARALHA E CRIA UMA CHAVE
-//     key = ftok("/sys", 'x');
-
-//     // PROXIMO PASSO CRIAÇÃO DE MEMORIA COMPARTILHADA
-
-//     // cria região de memoria compartilhada e retorna um id
-//     id = shmget(key, SHM_SIZE, 0644 | IPC_CREAT);
-
-//     // VINCULAR REGIÃO DE MEMORIA COMPARTILHADA A INTERFACE
-
-//     Data = (char *)shmat(id, 0, 0);
-
-//     if (argc > 1 && !strcmp(argv[1], "apagar"))
-//     {
-//         /* SE DIGITARMOS APAGAR ELE APAGA A REGIÃO DE MEMORIA */
-
-//         // DESVINCULAR DO PONTEIRO
-//         shmdt(Data);
-
-//         // DESTRUIR REGIÃO DE MEMORIA COMPARTILHADA
-//         shmctl(id, IPC_RMID, NULL);
-//         printf("DELETANDO SEGUIMENTO");
-
-//         exit(0);
-//     }
-
-//     if (argc == 2)
-//     {
-//         /* code */
-//         printf("ESCREVENDO NO SEGUIMENTO");
-//         strncpy(Data, argv[1], SHM_SIZE);
-//     }
-//     else
-//     {
-//         printf("CONTEUDO DO SEGUIMENTO %s\n", Data);
-//     }
-
-//     /*
-//     a2150336@pxe-ubuntu:~/output$ ./"memoriacompartilhada"
-//     CONTEUDO DO SEGUIMENTO
-//     a2150336@pxe-ubuntu:~/output$ ./memoriacompartilhada roda
-//     ESCREVENDO NO SEGUIMENTOa2150336@pxe-ubuntu:~/output$ ./memoriacompartilhada
-//     CONTEUDO DO SEGUIMENTO roda
-//     a2150336@pxe-ubuntu:~/output$ ./memoriacompartilhada apagar
-//     DELETANDO SEGUIMENTOa2150336@pxe-ubuntu:~/output$ ./memoriacompartilhada
-//     CONTEUDO DO SEGUIMENTO
-//     a2150336@pxe-ubuntu:~/output$ ./memoriacompartilhada
-//     CONTEUDO DO SEGUIMENTO
-//     a2150336@pxe-ubuntu:~/output$ ./memoriacompartilhada oi
-//     ESCREVENDO NO SEGUIMENTOa2150336@pxe-ubuntu:~/output$ ./memoriacompartilhada
-//     CONTEUDO DO SEGUIMENTO oi
-//     a2150336@pxe-ubuntu:~/output$ ./memoriacompartilhada apagar
-//     DELETANDO SEGUIMENTO
-//     */
-//     return 0;
-// }
